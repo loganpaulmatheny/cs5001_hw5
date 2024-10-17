9901664afef81c8cae5bc831b3234e0a8b39cb1b
Logan Matheny <loganpaulmatheny@gmail.com>
2024-10-17 17:47:40 -0400

    Save changes before starting substitute

--- remix_master.py
+++ remix_master.py
@@ -1,1 +1,18 @@
-# testing git autocomplete
+"""
+CS5001
+HW5 - remix_master
+Fall 2024
+Logan Matheny
+"""
+
+def substitute(song: list, old_word: str, new_word: str -> bool):
+    pass
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
