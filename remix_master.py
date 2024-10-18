fe6edba6adce9063c5761fe1de472bd461072adf
Logan Matheny <loganpaulmatheny@gmail.com>
2024-10-18 14:45:02 -0400

    Remove punctuation from string in substitute

--- remix_master.py
+++ remix_master.py
@@ -6,6 +6,7 @@ Logan Matheny
 """
 
 import music
+import re
 
 
 def substitute(song: list, old_word: str, new_word: str) -> bool:
@@ -19,16 +20,16 @@ def substitute(song: list, old_word: str, new_word: str) -> bool:
     for i in range(len(song)):
         if song[i].find(old_word) != -1:
             song[i] = song[i].replace(old_word, new_word)
+            song[i] = re.sub(r'[^\w\s]', '', song[i])
             found = True
         continue
     if found is False:
         print(f"Error: It seems the word {old_word} is not in "
-              "the song, try again.")
+              "the song, try again.") 
     return found
 
-
 def main():
-    print(substitute(music.SONGS[0], "dart", "fart"))
+    print(substitute(music.SONGS[0], "old", "fart"))
     print(music.SONGS[0])
 
 
