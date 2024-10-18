2aa586053325949e4af8e9090676def832175036
Logan Matheny <loganpaulmatheny@gmail.com>
2024-10-18 12:04:54 -0400

    Complete base substitute function

--- remix_master.py
+++ remix_master.py
@@ -5,13 +5,31 @@ Fall 2024
 Logan Matheny
 """
 
-def substitute(song: list, old_word: str, new_word: str -> bool):
-    pass
+import music
 
+
+def substitute(song: list, old_word: str, new_word: str) -> bool:
+    # Takes a song, word to look for, replace the word with new word
+    # List of strings
+    # Should iterate through the list and use .replace
+    """
+    FILL OUT LATER
+    """
+    found = False
+    for i in range(len(song)):
+        if song[i].find(old_word) != -1:
+            song[i] = song[i].replace(old_word, new_word)
+            found = True
+        continue
+    if found is False:
+        print(f"Error: It seems the word {old_word} is not in "
+              "the song, try again.")
+    return found
 
 
 def main():
-    pass
+    print(substitute(music.SONGS[0], "dart", "fart"))
+    print(music.SONGS[0])
 
 
 if __name__ == "__main__":
