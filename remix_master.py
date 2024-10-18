83f56c9ff132f7cb37f3e40a997258bab81cd824
Logan Matheny <loganpaulmatheny@gmail.com>
2024-10-18 14:52:15 -0400

    Complete substitute()

--- remix_master.py
+++ remix_master.py
@@ -14,23 +14,36 @@ def substitute(song: list, old_word: str, new_word: str) -> bool:
     # List of strings
     # Should iterate through the list and use .replace
     """
-    FILL OUT LATER
+    Function: Takes a song, which has been broken up into a list of phrases,
+        along with a word to 'look for' and word to replace it with if it
+        is found. It then modifies the song to replace the old word with
+        the new AND removes all the punctuation of within the song.
+    Return: Boolean based on any of the 'old word' were found within the song
+
     """
+    # Flag
     found = False
+
+    # Loop to check if old word is in song
+    # If any old word NOT not found (found)
+    # Both replace the new word and remove punctuation
     for i in range(len(song)):
         if song[i].find(old_word) != -1:
             song[i] = song[i].replace(old_word, new_word)
-            song[i] = re.sub(r'[^\w\s]', '', song[i])
+            song[i] = re.sub(r"[^\w\s]", "", song[i])
             found = True
         continue
+
+    # If no old words are found, print an error
     if found is False:
-        print(f"Error: It seems the word {old_word} is not in "
-              "the song, try again.") 
+        print(f"Error: It seems the word {old_word} is 
+              not in " "the song, try again.")
     return found
 
+
 def main():
-    print(substitute(music.SONGS[0], "old", "fart"))
-    print(music.SONGS[0])
+    # print(substitute(music.SONGS[0], "new", "fart"))
+    # print(music.SONGS[0])
 
 
 if __name__ == "__main__":
